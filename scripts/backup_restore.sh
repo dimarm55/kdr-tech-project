19c57a2bb8c4cbc0f4aaa362991d9185a01d2414
root <root@kdr-tech.cfd>
2025-05-06 20:48:44 +0000

    Modified restore script

--- scripts/backup_restore.sh
+++ scripts/backup_restore.sh
@@ -9,17 +9,17 @@ read copy
 if ls "$FILES_BACKUP_STORAGE"/*_"$copy"* >/dev/null 2>&1 && \
    ls "$MYSQL_BACKUP_STORAGE"/*_"$copy"* >/dev/null 2>&1; then
         cowsay "Begin restoration"
-	podman-compose stop 
+	podman-compose stop >/dev/null
 
 	rm -rf /app/data/html/
 	rm -rf /app/data/mysql/*
 
-	tar -xvf $FILES_BACKUP_STORAGE/kdr_tech_$copy.tar.gz -C /app/data/
+	tar -xf $FILES_BACKUP_STORAGE/kdr_tech_$copy.tar.gz -C /app/data/
 
-	podman-compose start
+	podman-compose start >/dev/null
 	
-	cowsay "Waiting 15 sec for mysql activation"
-	sleep 15
+	cowsay "Waiting 20 sec for mysql activation"
+	sleep 20
 	gunzip -ck $MYSQL_BACKUP_STORAGE/kdr_tech_$copy.sql.gz | podman exec -i db mysql -u root kdr_tech
 
 	cowsay "Restoration has finished!!!"
