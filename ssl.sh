82709cc90df1128950a2434df392f82e940e837f
root <root@kdr-tech.cfd>
2025-05-25 11:05:40 +0000

    Deleted website files and some updates

--- ssl.sh
+++ ssl.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-python3 -m http.server 80 --directory ./data/html &
+python3 -m http.server 80 --directory /var/www/html &
 pid=$!
 
-certbot certonly --webroot -w ./data/html -d kdr-tech.cfd -d www.kdr-tech.cfd
+certbot certonly --webroot -w /var/www/html -d kdr-tech.cfd -d www.kdr-tech.cfd -d roundcube.kdr-tech.cfd
 
 kill $pid
